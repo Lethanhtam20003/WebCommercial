d62974ca38c68a740e047b0d4511479730320881
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-03-27 00:01:56 +0700

    new: thêm file để dành găn label, với tên loi

---
+++ frontend/src/app/constants/error.message.ts
@@ -0,0 +1,7 @@
+export class ErrorMessage{
+  pleaseInputUsername: string = 'Vui lòng nhập tài khoản';
+  invalidEmail: string = 'Email không hợp lệ';
+  pleaseInputPassword: string = 'Vui lòng nhập mật khẩu';
+  passwordNotMatch: string = 'Mật khẩu không khớp';
+  pleaseInputPhoneNumber: string = 'Vui lòng nhập số điện thoại';
+}
---
+++ frontend/src/app/constants/label.ts
@@ -0,0 +1,11 @@
+export class Labels {
+  username: string = 'Tên người dùng';
+  password: string = 'Mật khẩu';
+  email: string = 'Email';
+  phoneNumber: string = 'Số điện thoại';
+  confirmPassword: string = 'Nhập lại mật khẩu';
+  signUp: string = 'Đăng ký';
+  login: string = 'Đăng nhập';
+  acceptThe: string = 'Tôi đồng ý với các ';
+  arrangement: string = 'điều khoản';
+}
