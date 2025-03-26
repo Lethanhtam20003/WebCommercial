048665771f49a78ee93e6b1a670570df54bc8c87
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-03-27 00:00:57 +0700

    update: cấu hình liên quan tới thư viện ng-zorro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {LoginPage} from './pages/login/login.page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    LoginPage
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
 }
