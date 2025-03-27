343adb0b0e67b2b5afe4b1ad1ecc881f1f575d2d
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-03-27 21:52:28 +0700

    new: create label and message log file

---
+++ frontend/src/app/constant/error.message.ts
@@ -0,0 +1,6 @@
+export class ErrorMessage {
+  pleaseEnterUsername: string='Vui lòng nhập tài khoản';
+  pleaseEnterPassword: string='Vui lòng nhập mật khẩu';
+  pleaseEnterConfirmPassword: string='Vui lòng nhập lại mật khẩu';
+  pleaseEnterEmail: string='Vui lòng nhập email';
+}
---
+++ frontend/src/app/constant/label.ts
@@ -0,0 +1,10 @@
+export class Label {
+  logIn: string='Đăng nhập';
+  register: string='Đăng ký';
+  forgotPassword: string='Quên mật khẩu';
+  username: string='Tên đăng nhập';
+  password: string='Mật khẩu';
+  confirmPassword: string='Xác nhận mật khẩu';
+  email: string='Email';
+  phone: string='Số điện thoại';
+}
