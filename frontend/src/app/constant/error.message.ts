72674648619890894f51adc903680040051d5690
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-01 18:39:07 +0700

    update: update message file

--- frontend/src/app/constant/error.message.ts
+++ frontend/src/app/constant/error.message.ts
@@ -3,4 +3,6 @@ export class ErrorMessage {
   pleaseEnterPassword: string='Vui lòng nhập mật khẩu';
   pleaseEnterConfirmPassword: string='Vui lòng nhập lại mật khẩu';
   pleaseEnterEmail: string='Vui lòng nhập email';
+  usernameHasAtLeast3Characters: string='Tài khoản phải có ít nhất 3 ký tự';
+  passwordHasAtLeast8CharactersAndSmallerThan30: string='Mật khẩu phải có ít nhất 8 ký tự và phải có nhỏ hơn 30 ký tự';
 }
