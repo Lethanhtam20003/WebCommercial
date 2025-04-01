f05dc73c663a3b525b7ee3a928d3ede530542a65
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-01 18:37:43 +0700

    update: remove unnecessary file

--- frontend/src/app/constants/error.message.ts
+++
@@ -1,7 +0,0 @@
-export class ErrorMessage{
-  pleaseInputUsername: string = 'Vui lòng nhập tài khoản';
-  invalidEmail: string = 'Email không hợp lệ';
-  pleaseInputPassword: string = 'Vui lòng nhập mật khẩu';
-  passwordNotMatch: string = 'Mật khẩu không khớp';
-  pleaseInputPhoneNumber: string = 'Vui lòng nhập số điện thoại';
-}
