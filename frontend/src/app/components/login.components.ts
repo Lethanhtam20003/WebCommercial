6454699e2a6c7a28ef0913fff14f316f01ecd700
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-01 18:39:18 +0700

    update: update login component

--- frontend/src/app/components/login.components.ts
+++ frontend/src/app/components/login.components.ts
@@ -20,73 +20,110 @@ import { NgClass } from '@angular/common';
 		NgClass,
 	],
 	template: `
-		<div class="h-screen w-full flex items-start justify-center pt-10 bg-[#FF7F50]">
-			<form class="bg-white border border-gray-200 drop-shadow-2xl p-10 rounded-xl">
-				<div class=" flex justify-center font-bold pb-10 text-[40px]">
-					{{ labels.logIn }}
-				</div>
-				<div class="card flex justify-center flex-col w-100">
-					<div class="flex flex-col justify-center pb-5">
-						<p-floatlabel variant="on" class="w-full">
-							<input
-                #usernameInput
-								pInputText
-								pSize="large"
-								class="w-full"
-								id="{{ usernameInputId }}"
-								[(ngModel)]="username"
-								autocomplete="on"
-								name="{{ usernameInputId }}"
-							/>
-							<label
-								[ngClass]="{ '!text-black': usernameIsFocused }"
-								for="{{ usernameInputId }}"
-								>{{ labels.username }}</label
-							>
-						</p-floatlabel>
-						<small class="block mt-1 text-sm text-gray-500 pl-3"
-							>{{ errorMessage.pleaseEnterUsername }}
-						</small>
-					</div>
-					<div class="flex flex-col justify-center">
-						<p-floatlabel variant="on" class="w-full">
-							<p-password
-								[(ngModel)]="password"
-								class="w-full"
-								size="large"
-								[toggleMask]="true"
-								[feedback]="false"
-								inputId="on_label"
-								name="{{ passwordInputId }}"
-								(onFocus)="setFocus('password', true)"
-								(onBlur)="setFocus('password', false)"
-							/>
-							<label
-								[ngClass]="{ 'text-black': passwordIsFocused }"
-								for="{{ passwordInputId }}"
-								>{{ labels.password }}</label
-							>
-						</p-floatlabel>
-						<small class="block mt-1 text-sm text-gray-500 pl-3"
-							>{{ errorMessage.pleaseEnterPassword }}
-						</small>
-					</div>
-					<div class="card flex justify-center gap-4 pt-4">
-						<p-button
-							label="{{ labels.logIn }}"
-							icon="pi pi-check"
-							[loading]="loading"
-							(onClick)="load()"
-							[style]="{
-								'background-color': '#E9762B',
-								'border-color': '#E9762B',
+      <div class="h-screen w-full flex items-start justify-center pt-10 bg-white">
+        <form class="bg-white border border-gray-200 drop-shadow-2xl p-10 rounded-md">
+          <div class=" flex justify-center font-bold pb-10 text-[40px]">
+            {{ labels.logIn }}
+          </div>
+          <div class="card flex justify-center flex-col w-100">
+            <div class="flex flex-col justify-center pb-5">
+              <p-floatlabel variant="on" class="w-full">
+                <input
+                  #usernameInput
+                  pInputText
+                  pSize="large"
+                  class="w-full"
+                  id="{{ usernameInputId }}"
+                  [(ngModel)]="username"
+                  autocomplete="on"
+                  name="{{ usernameInputId }}"
+                />
+                <label
+                  [ngClass]="{ '!text-black': usernameIsFocused }"
+                  for="{{ usernameInputId }}"
+                >{{ labels.username }}</label
+                >
+              </p-floatlabel>
+              @if (username == null || username == '') {
+                <small class="block mt-1 text-sm text-red-500 pl-1"
+                >{{ errorMessage.pleaseEnterUsername }}
+                </small>
+              } @else if (username.length < 3) {
+                <small class="block mt-1 text-sm text-red-500 pl-1"
+                >{{ errorMessage.usernameHasAtLeast3Characters }}
+                </small>
+              }
+            </div>
+            <div class="flex flex-col justify-center">
+              <p-floatlabel variant="on" class="w-full">
+                <p-password
+                  [(ngModel)]="password"
+                  class="w-full"
+                  size="large"
+                  [toggleMask]="true"
+                  [feedback]="false"
+                  inputId="on_label"
+                  name="{{ passwordInputId }}"
+                  (onFocus)="setFocus('password', true)"
+                  (onBlur)="setFocus('password', false)"
+                />
+                <label
+                  [ngClass]="{ 'text-black': passwordIsFocused }"
+                  for="{{ passwordInputId }}"
+                >{{ labels.password }}</label
+                >
+              </p-floatlabel>
+              @if (password == null || password == '') {
+                <small class="block mt-1 text-sm text-red-500 pl-3"
+                >{{ errorMessage.pleaseEnterPassword }}
+                </small>
+              } @else if (password.length < 8 || password.length > 30) {
+                <small class="block mt-1 text-sm text-red-500 pl-3"
+                >{{ errorMessage.passwordHasAtLeast8CharactersAndSmallerThan30 }}
+                </small>
+              }
+            </div>
+            <div class="card flex gap-4 pt-4">
+              <p-button
+                label="{{ labels.logIn }}"
+                [style]="{
+								'background-color': '#141d22',
+								'border-color': '#141d22',
+								'border-radius': '0px',
 							}"
-						/>
-					</div>
-				</div>
-			</form>
-		</div>
-	`,
+              />
+              <p-button
+                label="{{ labels.register }}"
+                [style]="{
+								'background-color': 'black',
+								'border-color': 'black',
+								'border-radius': '0px',
+							}"
+              />
+            </div>
+            <div class="card flex flex-col gap-2 pt-2">
+              <p class="text-center text-sm">{{labels.loginWithOther}}</p>
+              <div class="card flex flex-row gap-2 w-full justify-center items-center">
+                <p-button
+                  severity="info"
+                  [rounded]="true"
+                  icon="pi pi-facebook"/>
+                <p-button
+                  severity="success"
+                  [rounded]="true"
+                  icon="pi pi-google"/>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    `,
+  styles: `
+    ::ng-deep .p-button:hover {
+      background-color: #FF4500 !important;
+      border-color: #FF4500 !important;
+    }
+  `
 })
 export class LoginComponent {
   /*
@@ -94,11 +131,6 @@ export class LoginComponent {
   * */
 	username: string='';
 	password: string='';
-
-  /*
-  * @description: loading state for button
-  * */
-	loading: boolean = false;
 
   /*
   * @description: id for username and password input
@@ -117,17 +149,6 @@ export class LoginComponent {
   * */
 	labels: Label = new Label();
 	errorMessage: ErrorMessage = new ErrorMessage();
-
-  /*
-  * @description: constructor for component
-  * */
-	load() {
-		this.loading = true;
-
-		setTimeout(() => {
-			this.loading = false;
-		}, 2000);
-	}
 
   /*
   * @description: set focus for username input
@@ -153,8 +174,5 @@ export class LoginComponent {
 		} else if (field === 'password') {
 			this.passwordIsFocused = isFocused;
 		}
-		console.log(
-			`usernameIsFocused: ${this.usernameIsFocused}, passwordIsFocused: ${this.passwordIsFocused}`
-		);
 	}
 }
