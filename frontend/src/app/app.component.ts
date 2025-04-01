0fbca4cf3b72536094f05dbd298800ed08df6b91
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-01 18:38:24 +0700

    update: apply router-outlet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ButtonModule} from 'primeng/button';
 import {LoginPages} from './pages/login.pages';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, LoginPages,],
-  templateUrl: './app.component.html',
+  imports: [ButtonModule, RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './components/login.components';
+import {Label} from './constant/label';
 
-export const routes: Routes = [];
+const label: Label=new Label();
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    title: label.logIn,
+  },
+];
