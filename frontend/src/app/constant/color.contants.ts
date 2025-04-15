fcf50692e56464b88ed779f7851c5bafffbd7caf
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-04-15 10:46:25 +0700

    update constant file

---
+++ frontend/src/app/constant/color.contants.ts
@@ -0,0 +1,10 @@
+export class ColorPalette {
+  public static readonly Grey: string = "#cbd5e1";
+	public static readonly OrangeRed: string = "#FF4500";
+	public static readonly StrongRed: string = "#cf081f";
+	public static readonly BlackPearl: string = "#141d22";
+	public static readonly BrightGrey: string = "#575a63"
+	public static readonly FacebookBlue: string = "#1877F2";
+	public static readonly GoogleGreen: string = "#0F9D58";
+  public static readonly BrightBlue: string = "#0096FF";
+}
--- frontend/src/app/constant/selectorName.ts
+++ frontend/src/app/constant/selectorName.ts
@@ -3,4 +3,6 @@ export class selectorName {
   // static readonly loginPage:string= 'login-page';
   static readonly registerComponent:string= 'register-component';
   // static readonly registerPage:string= 'register-page';
+  static readonly headerTopBar:string= 'header-top-bar';
+  static readonly footer:string= 'footer';
 }
