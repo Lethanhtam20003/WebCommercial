0cf7047a9a8f7971fffae4de5f1dbcd0b3bad173
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-05-10 14:43:09 +0700

    initial default value for form

--- frontend/src/app/shared/components/user-profile/user-profile.component.ts
+++ frontend/src/app/shared/components/user-profile/user-profile.component.ts
@@ -1,34 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { LabelConstants } from '../../../core/constants/label.constants';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+	FormBuilder,
+	FormControl,
+	FormControlState,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
 import Swal from 'sweetalert2';
 import { ErrorMessageConstants } from '../../../core/constants/error-message.constants';
+import { CommonModule } from '@angular/common';
+import {
+	UserProfileFormFields,
+} from './user-profile.interface';
 
 @Component({
 	selector: 'user-profile',
-	imports: [],
+	imports: [CommonModule, ReactiveFormsModule],
 	templateUrl: './user-profile.component.html',
 	styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent implements OnInit {
-	uploadImageForm!: FormGroup;
+	updateInformationForm!: FormGroup;
 	imagePreview: string | ArrayBuffer | null = null;
 	protected readonly label = LabelConstants;
 
 	constructor(private fb: FormBuilder) {}
+
 	ngOnInit(): void {
-		this.uploadImageForm = this.fb.group({
-			profileImage: [null],
-		});
+		this.updateInformationForm = this.fb.group({
+			username: new FormControl<string>({ value: 'username', disabled: true }),
+			email: new FormControl<string>('name@example.com'),
+			fullName: new FormControl<string>('full-name'),
+			profileImage: new FormControl<File | null>(null),
+      address: new FormControl<string>('San Francisco, CA'),
+      phoneNum: new FormControl<string>('555-123-4567'),
+      birthday: new FormControl<string>('06/10/1988'),
+      gender: new FormControl<string>(this.label.male)
+		}) as FormGroup<UserProfileFormFields>;
 	}
 
 	onFileSelected(event: Event) {
 		const file = (event.target as HTMLInputElement).files?.[0];
 		if (!file) return;
 
+		console.log(file.name);
+
 		const maxSizeInMB = 5;
 		const maxSizeInBytes = maxSizeInMB * 1024 * 1024;
-		const allowedTypes = ['images/png', 'images/jpeg'];
+		const allowedTypes = ['image/png', 'image/jpeg'];
 
 		if (file.size > maxSizeInBytes) {
 			Swal.fire({
@@ -37,7 +58,7 @@ export class UserProfileComponent implements OnInit {
 				text: ErrorMessageConstants.imageIsOversized + '!',
 			});
 
-			this.uploadImageForm.patchValue({ profileImage: null });
+			this.updateInformationForm.patchValue({ profileImage: null });
 			this.imagePreview = null;
 			return;
 		}
@@ -49,13 +70,13 @@ export class UserProfileComponent implements OnInit {
 				text: ErrorMessageConstants.imageIsNotInAllowedType + '!',
 			});
 
-			this.uploadImageForm.patchValue({ profileImage: null });
+			this.updateInformationForm.patchValue({ profileImage: null });
 			this.imagePreview = null;
 			return;
 		}
 
-		this.uploadImageForm.patchValue({ profileImage: file });
-		this.uploadImageForm.get('profileImage')?.updateValueAndValidity();
+		this.updateInformationForm.patchValue({ profileImage: file });
+		this.updateInformationForm.get('profileImage')?.updateValueAndValidity();
 
 		const reader = new FileReader();
 		reader.onload = () => {
