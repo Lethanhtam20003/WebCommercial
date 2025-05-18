b176161413843bb8d3d9c8d49cd9754e58276061
Lethanhtam20003 <tam1442k3@gmail.com>
2025-05-18 16:58:14 +0700

    Add new error codes for token validation and update authentication logic to handle token expiration

--- frontend/src/app/core/service/auth.service.ts
+++ frontend/src/app/core/service/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
               private alertService: AlertService
   ) {
     // Kiểm tra trạng thái đăng nhập khi service được khởi tạo
-    // this.checkInitialAuthState();
+    this.checkInitialAuthState();
   }
 
   /**
@@ -32,8 +32,10 @@ export class AuthService {
   private checkInitialAuthState(): void {
     const token = this.getStoredToken();
     if (token) {
-      this.verifyAuthentication().subscribe();
-    }
+    this.validateToken(token).subscribe(); // sẽ tự cập nhật login status
+  } else {
+    this.updateLoginStatus(false);
+  }
   }
 
   /**
@@ -65,15 +67,15 @@ export class AuthService {
           return of(true); // Đã đăng nhập, không cần kiểm tra lại
         } else {
           // Return false if not logged in
-          return this.validateToken();
+          const token = this.getStoredToken();
+          return this.validateToken(token);
         }
       })
     );
   }
 
-  validateToken(): Observable<boolean> {
+  validateToken(token: string | null): Observable<boolean> {
     // Kiểm tra xem token có tồn tại không
-    const token = this.getStoredToken();
     if (!token) {
       this.updateLoginStatus(false);
       return of(false);
@@ -90,15 +92,20 @@ export class AuthService {
     })
     .pipe(
       switchMap(res => {
-        if(res.code === 200){
-          const isAuthenticated = res.result.isValid ;
-          this.updateLoginStatus(isAuthenticated);
-          return of(true);
-        }
-        // if (isAuthenticated && !res.result.isValid) {
-        //   return this.handleTokenRefresh();
-        // }
+        if (res.code === 200) {
+          const isAuthenticated = res.result.valid;
 
+          if (isAuthenticated) {
+            this.updateLoginStatus(true);
+            return of(true);
+          }
+
+          // Nếu token không hợp lệ, thử làm mới
+          return this.handleTokenRefresh();
+        }
+
+        // Các code khác không phải 200
+        this.updateLoginStatus(false);
         return of(false);
       }),
       catchError(() => {
@@ -115,32 +122,36 @@ export class AuthService {
    * @returns Observable<boolean>
    * @private
    */
-  // private handleTokenRefresh(): Observable<boolean> {
-  //   const token = this.getStoredToken();
-  //   if (!token) {
-  //     return of(false);
-  //   }
+  private handleTokenRefresh(): Observable<boolean> {
+    const token = this.getStoredToken();
+    if (!token) {
+      return of(false);
+    }
 
-  //   return this.http.post<ApiResponse<AuthenticationResponse>>(
-  //     URL_API.refreshTokenUrl,
-  //     { token }
-  //   ).pipe(
-  //     map(res => {
-  //       const isSuccess = res.code === 200;
-  //       if (isSuccess) {
-  //         localStorage.setItem(this.TOKEN_KEY, res.result.access_token);
-  //         this.updateLoginStatus(true);
-  //       } else {
-  //         this.updateLoginStatus(false);
-  //       }
-  //       return isSuccess;
-  //     }),
-  //     catchError(() => {
-  //       this.updateLoginStatus(false);
-  //       return of(false);
-  //     })
-  //   );
-  // }
+    return this.http.post<ApiResponse<AuthenticationResponse>>(
+      URL_API.refreshTokenUrl,
+      { token },
+      {
+        headers: new HttpHeaders().set('skipAuth', 'true'),
+      }
+     
+    ).pipe(
+      map(res => {
+        const isSuccess = res.code === 200;
+        if (isSuccess) {
+          localStorage.setItem(this.TOKEN_KEY, res.result.token);
+          this.updateLoginStatus(true);
+        } else {
+          this.updateLoginStatus(false);
+        }
+        return isSuccess;
+      }),
+      catchError(() => {
+        this.updateLoginStatus(false);
+        return of(false);
+      })
+    );
+  }
 
   /**
    * Đăng nhập bằng Facebook
@@ -215,9 +226,7 @@ export class AuthService {
         headers: new HttpHeaders().set('skipAuth', 'true'),
       }
     ).pipe(
-    
       catchError((error: HttpErrorResponse) => {
-        console.error('Error occurred during registration:', error);
         this.updateLoginStatus(false);
           let errorMessage: string = ErrorMessageConstants.UnknownErrorOccurred;
 
