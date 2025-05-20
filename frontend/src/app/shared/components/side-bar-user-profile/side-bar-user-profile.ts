5b82516776bbfc409f109c7aab591b94a3e36aec
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-05-20 17:28:25 +0700

    change label of side bar, adding hight light color when change access to the user/profile site, change color of dropdown item when active

--- frontend/src/app/shared/components/side-bar-user-profile/side-bar-user-profile.ts
+++ frontend/src/app/shared/components/side-bar-user-profile/side-bar-user-profile.ts
@@ -1,17 +1,35 @@
-import { Component } from "@angular/core";
-import { LabelConstants } from "../../../core/constants/label.constants";
-import { RouterLink } from "@angular/router";
-import { RouteLink as Routes } from "../../../core/constants/route-link";
+import { Component, OnInit } from '@angular/core';
+import { Label } from '../../../core/constants/label.constants';
+import {
+	NavigationEnd,
+	Router,
+	RouterLink,
+	RouterLinkActive,
+} from '@angular/router';
+import { RouteLink as Routes } from '../../../core/constants/route-link';
+import { filter } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'side-bar-user-profile',
-  standalone: true,
-  imports: [RouterLink],
-  styleUrl: './side-bar-user-profile.scss',
-  templateUrl: './side-bar-user-profile.html',
+	selector: 'side-bar-user-profile',
+	standalone: true,
+	imports: [RouterLink, NgClass],
+	styleUrl: './side-bar-user-profile.scss',
+	templateUrl: './side-bar-user-profile.html',
 })
 export class SideBarUserProfile {
-  protected readonly label = LabelConstants;
-  protected readonly routes = Routes;
-
+	protected readonly label = Label;
+	protected readonly routes = Routes;
+	currentUrl = '';
+	constructor(private router: Router) {
+		this.router.events
+			.pipe(filter(event => event instanceof NavigationEnd))
+			.subscribe((event: NavigationEnd) => {
+				this.currentUrl = event.urlAfterRedirects;
+			});
+	}
+	isActive(path: string): boolean {
+		console.log(this.currentUrl);
+		return this.currentUrl === path;
+	}
 }
