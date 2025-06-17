62d93aba32bfb36e76ed438e83579d67d486e659
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-06-17 21:11:14 +0700

    update backend category endpoint

--- backend/src/main/java/com/nlu/WebThuongMai/controller/CategoryController.java
+++ backend/src/main/java/com/nlu/WebThuongMai/controller/CategoryController.java
@@ -1,14 +1,16 @@
 package com.nlu.WebThuongMai.controller;
 
+import com.nlu.WebThuongMai.dto.request.categoryReq.CategoriesAdminFilterRequest;
+import com.nlu.WebThuongMai.dto.response.ApiResponse;
 import com.nlu.WebThuongMai.dto.response.productResp.CategoryResponse;
 import com.nlu.WebThuongMai.service.CategoryService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +33,15 @@ public class CategoryController {
         return categoryService.getCategoryById(id);
     }
 
+    @PostMapping("/admin/filter")
+    public ApiResponse<Page<CategoryResponse>> filterCategoriesAdmin(
+            @RequestBody CategoriesAdminFilterRequest request,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ApiResponse.<Page<CategoryResponse>>builder()
+                .result(categoryService.filterCategories(request, pageable))
+                .build();
+    }
 }
